a6e9d0f787528050e35d9a95346e14e83b6d53d4
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-22 11:42:52 +0300

    created vidly project

---
+++ vidly/src/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
