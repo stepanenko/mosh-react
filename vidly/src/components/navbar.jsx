10c33b2e901f1306340f436d75d19357e4d3db9e
stepanenko <serhiy.stepanenko@aiesec.net>
2020-10-29 19:05:33 +0200

    progress

--- vidly/src/components/navbar.jsx
+++ vidly/src/components/navbar.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="navbar-brand" to='/'>VIDLY</NavLink>
+      <Link className="navbar-brand" to='/'>VIDLY</Link>
       <div className="navbar-nav">
         <NavLink className="nav-item nav-link" to='/movies'>Movies</NavLink>
         <NavLink className="nav-item nav-link" to='/customers'>Customers</NavLink>
