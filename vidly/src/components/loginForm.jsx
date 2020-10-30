f517089f96238e48d614249a1f5ff6b3967ff289
stepanenko <serhiy.stepanenko@aiesec.net>
2020-10-30 17:15:45 +0200

    handle input changes

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,24 +1,47 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginForm = () => {
+  const [name, setName] = useState('');
+  const [pass, setPass] = useState('');
+  
   const submit = React.createRef();
 
   const handleSubmit = ev => {
     ev.preventDefault();
+    // console.log(submit.current.value);
+    // console.log(pass);
+  }
 
-    console.log(submit.current.value);
+  const handleChange = ({ currentTarget: input }) => {
+    input.name === 'password'
+      ? setPass(input.value)
+      : setName(input.value);
+
+      console.log(name, pass);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="form-group">
         <label htmlFor="username">Username</label>
-        <input type="login" autoFocus ref={submit} className="form-control" id="username" placeholder="Your username" />
+        <input type="login" className="form-control" autoFocus
+          ref={submit}
+          onChange={handleChange}
+          id="username"
+          name="username"
+          placeholder="Your username"
+        />
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
-        <input type="password" className="form-control" id="password" placeholder="Password" />
+        <input type="password" className="form-control" 
+          value={pass}
+          onChange={handleChange}
+          id="password"
+          name="password"
+          placeholder="Your password"
+        />
       </div>
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
