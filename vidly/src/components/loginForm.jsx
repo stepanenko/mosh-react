9a5d528c68e88fa4b1c1aefe41e19b1f4f6b723c
stepanenko <serhiy.stepanenko@aiesec.net>
2020-10-31 15:37:46 +0200

    used Joi for validation

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,22 +1,33 @@
 
 import React, { useState } from 'react';
+import Joi from 'joi-browser';
 
 import Input from './common/input';
 
 const LoginForm = () => {
-  const [login, setName] = useState('');
-  const [password, setPass] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [errors, setErrors] = useState({});
 
-  const validate = () => {
-    const errorsObj = {};
-
-    if (login.trim() === '') errorsObj.login = 'Login is required';
-    if (password.trim() === '') errorsObj.password = 'Password is required';
-
-    return Object.keys(errorsObj).length === 0 ? null : errorsObj;
+  const userSchema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password')
   }
-  
+
+  const validate = () => {
+    const options = { abortEarly: false };
+    const user = { username, password };
+    const { error } = Joi.validate(user, userSchema, options);
+    console.log('JOI', error.details);
+
+    if (!error) return null;
+
+    const errorsObj = {};
+    for (let item of error.details)
+      errorsObj[item.path[0]] = item.message;
+    return errorsObj;
+  }
+
   const handleSubmit = ev => {
     ev.preventDefault();
     const err = validate();
@@ -26,17 +37,17 @@ const LoginForm = () => {
       console.log('ERR', errors);
       return;
     }
-    console.log('Logged In', { name: login, pass: password });
+    console.log('Logged In', { username, password });
   }
 
   const handleChange = ({ currentTarget: input }) => {
-    input.name === 'password' ? setPass(input.value) : setName(input.value);
-    console.log('GOT IT', login, password);
+    input.name === 'password' ? setPassword(input.value) : setUsername(input.value);
+    console.log('GOT IT', username, password);
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <Input field='username' type='login' onInputChange={handleChange} error={errors.login} />
+      <Input field='username' type='login' onInputChange={handleChange} error={errors.username} />
       <Input field='password' type='password' onInputChange={handleChange} error={errors.password} />
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
