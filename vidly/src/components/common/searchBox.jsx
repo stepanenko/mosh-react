73bb08d9c8129337d8afaa36277211603684d8b6
stepanenko <serhiy.stepanenko@aiesec.net>
2020-11-07 17:43:02 +0200

    search done

--- vidly/src/components/common/searchBox.jsx
+++ vidly/src/components/common/searchBox.jsx
@@ -3,20 +3,14 @@ import React from 'react';
 
 const SearchBox = ({ value, onChange }) => {
 
-  const handleChange = ({ currentTarget: input }) => {
-    onChange(input.value);   // my solution
-  };
-
   return (
-    <div className="form-group">
-      <input type="text"
-        className="form-control"
-        id="search"
-        value={value}
-        placeholder="Search..."
-        onChange={handleChange}
-      />
-    </div>
+    <input className="form-control my-3"
+      name='query'
+      type="text"
+      value={value}
+      placeholder="Search..."
+      onChange={ev => onChange(ev.currentTarget.value)}
+    />
   );
 };
 
